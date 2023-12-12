e1e9965771ffb35c720148f52862abd9a43d6037
Gaelle <gaelle.joubert@api-k.com>
2023-12-12 17:06:46 +0100

    Fix issues 15 & 17 and Clean for V 3.0.3"

--- lib/dtmf.dart
+++ lib/dtmf.dart
@@ -9,24 +9,25 @@ class Dtmf {
   /// at the specified volume. If no volume is specified, the system default is used. Volume value should be between
   /// 0 and 1.
   /// Returns true if tone played successfully
+  /// Set ignoreDtmfSystemSettings to true, if you want to play sound even when dtmf sounds are disable on the device (ex : on a tablet)
+  /// Set forceMaxVolume to true if you want to increase max volume to the max volume of the phone, (and not just max volume of DTMF sounds).
+  /// If you force max Volume to true, you can still control the volume with the volume atribute.
   ///
   static Future<bool?> playTone(
       {required String digits,
-      int? durationMs,
-      double? samplingRate,
-      double? volume}) async {
-    if (samplingRate == null) {
-      samplingRate = 500;
-    }
-    if (durationMs == null) {
-      durationMs = 160;
-    }
+      int durationMs =160,
+      double samplingRate =500,
+      double volume = 1,
+      bool ignoreDtmfSystemSettings=false,
+      bool forceMaxVolume=false}) async {
+
     final Map<String, Object?> args = <String, dynamic>{
       "digits": digits,
       "samplingRate": samplingRate,
       "durationMs": durationMs,
-      "volume": volume
-    };
+      "volume": volume,
+      "ignoreDtmfSystemSettings":ignoreDtmfSystemSettings,
+      "forceMaxVolume":forceMaxVolume};
     return await _channel.invokeMethod('playTone', args);
   }
 }
